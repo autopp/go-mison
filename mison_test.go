1204ae83eb1f9e9f754609d5275109ab97031274
autopp <autopp.inc@gmail.com>
2019-04-24 12:41:47 +0900

    test: add test cases for bit operations

---
+++ mison_test.go
@@ -0,0 +1,65 @@
+package mison
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func bitsToUint64(bits string) uint64 {
+	var ret uint64
+	n := len(bits)
+	for i := 0; i < n; i++ {
+		ret <<= 1
+		if bits[i] == '1' {
+			ret |= 1
+		}
+	}
+
+	return ret
+}
+
+func TestRemoveRightmost1(t *testing.T) {
+	cases := []struct {
+		bits     string
+		expected string
+	}{
+		{bits: "11101000", expected: "11100000"},
+		{bits: "01111111", expected: "01111110"},
+		{bits: "00000000", expected: "00000000"},
+	}
+
+	for _, tt := range cases {
+		assert.Equal(t, bitsToUint64(tt.expected), RemoveRightmost1(bitsToUint64(tt.bits)))
+	}
+}
+
+func TestExtractRightmost1(t *testing.T) {
+	cases := []struct {
+		bits     string
+		expected string
+	}{
+		{bits: "11101000", expected: "00001000"},
+		{bits: "01111111", expected: "00000001"},
+		{bits: "00000000", expected: "00000000"},
+	}
+
+	for _, tt := range cases {
+		assert.Equal(t, bitsToUint64(tt.expected), ExtractRightmost1(bitsToUint64(tt.bits)))
+	}
+}
+
+func TestSmearRightmost1(t *testing.T) {
+	cases := []struct {
+		bits     string
+		expected string
+	}{
+		{bits: "11101000", expected: "00001111"},
+		{bits: "01111111", expected: "00000001"},
+		{bits: "00000000", expected: "00000000"},
+	}
+
+	for _, tt := range cases {
+		assert.Equal(t, bitsToUint64(tt.expected), SmearRightmost1(bitsToUint64(tt.bits)))
+	}
+}
