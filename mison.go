a3041e60b10185fcc2efea49d8ab54d978c90e17
autopp <autopp.inc@gmail.com>
2019-04-24 11:32:03 +0900

    initial commit

---
+++ mison.go
@@ -0,0 +1,31 @@
+package mison
+
+/*
+RemoveRightmost1 removes the rightmost 1 in x.
+
+E.g.
+	11101000 -> 11100000
+*/
+func RemoveRightmost1(x uint64) uint64 {
+	panic("not implemented")
+}
+
+/*
+ExtractRightmost1 extract the rightmost 1 in x.
+
+E.g.
+	11101000 -> 00001000
+*/
+func ExtractRightmost1(x uint64) uint64 {
+	panic("not implemented")
+}
+
+/*
+SmearRightmost1 extract the rightmost 1 in x and smear it to the right.
+
+E.g.
+	11101000 -> 00001111
+*/
+func SmearRightmost1(x uint64) uint64 {
+	panic("not implemented")
+}
